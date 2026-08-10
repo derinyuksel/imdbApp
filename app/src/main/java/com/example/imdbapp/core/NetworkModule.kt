3c0409a4eb473c5dd820f98ca0b01110e3fc8035
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-10 16:03:01 +0300

    Add NetworkModule for Retrofit and OkHttpClient configuration

---
+++ app/src/main/java/com/example/imdbapp/core/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.example.imdbapp.core
+
+import com.example.imdbapp.core.NetworkConstants.CONNECT_TIMEOUT
+import com.example.imdbapp.core.NetworkConstants.READ_TIMEOUT
+import com.example.imdbapp.core.NetworkConstants.WRITE_TIMEOUT
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule{
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient) : Retrofit{
+        return  Retrofit.Builder()
+            .baseUrl("https://api.example.com/")
+            .client(okHttpClient)
+            .addConverterFactory(
+                Json.asConverterFactory("application/json".toMediaType())
+            )
+            .build()
+    }
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+        isLenient = true
+    }
+
+    fun provideHttpClient(authInterceptor: AuthInterceptor) : OkHttpClient {
+        return  OkHttpClient.Builder()
+            .connectTimeout(CONNECT_TIMEOUT , TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(authInterceptor)
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .build()
+    }
+}
+
