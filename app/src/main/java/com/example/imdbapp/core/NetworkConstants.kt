48b7d743740e5c7c657da62f102c9edc7da42f4a
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-10 16:03:14 +0300

    Add NetworkConstants for base URL and timeout configurations

---
+++ app/src/main/java/com/example/imdbapp/core/NetworkConstants.kt
@@ -0,0 +1,11 @@
+package com.example.imdbapp.core
+
+object NetworkConstants {
+
+
+    const val BASE_URL = ""
+    const val CONNECT_TIMEOUT = 30L
+    const val READ_TIMEOUT = 30L
+    const val WRITE_TIMEOUT = 30L
+
+}
