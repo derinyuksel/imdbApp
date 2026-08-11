532402690203c668cd4f0c2a2285db9f9c8a8ae3
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-12 00:35:06 +0300

    Add api base URL in NetworkConstants

--- app/src/main/java/com/example/imdbapp/core/NetworkConstants.kt
+++ app/src/main/java/com/example/imdbapp/core/NetworkConstants.kt
@@ -3,7 +3,7 @@ package com.example.imdbapp.core
 object NetworkConstants {
 
 
-    const val BASE_URL = ""
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     const val CONNECT_TIMEOUT = 30L
     const val READ_TIMEOUT = 30L
     const val WRITE_TIMEOUT = 30L
