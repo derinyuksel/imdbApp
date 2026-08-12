d6c25dc3a7b459d1e5bad89fcec09e0db64a1bc5
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-12 10:02:57 +0300

    Add NetworkResult interface for network response handling

---
+++ app/src/main/java/com/example/imdbapp/core/NetworkResult.kt
@@ -0,0 +1,9 @@
+package com.example.imdbapp.core
+
+sealed interface NetworkResult<out T> {
+
+    data class Success<T>(val data: T) : NetworkResult<T>
+
+    data class Error(val message: String) : NetworkResult<Nothing>
+
+}
