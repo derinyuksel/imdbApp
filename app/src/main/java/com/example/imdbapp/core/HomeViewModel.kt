c1e3df87eddcabd09e2907b160783d62b038d4eb
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-12 10:31:40 +0300

    Add UI state management

--- app/src/main/java/com/example/imdbapp/core/HomeViewModel.kt
+++ app/src/main/java/com/example/imdbapp/core/HomeViewModel.kt
@@ -1,15 +1,33 @@
 package com.example.imdbapp.core
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.imdbapp.home.HomeUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    val repo : Repository
+    val repo: Repository
 ) : ViewModel() {
-}
 
-private val _uiState = MutableStateFlow(HomeUiState())
+
+    private val _uiState = MutableStateFlow(HomeUiState())
+    val uiState = _uiState.asStateFlow()
+
+
+    init {
+        loadTrendingMovies()
+    }
+
+    fun loadTrendingMovies(){
+        viewModelScope.launch{
+            _uiState.value = _uiState.value.copy(isLoading = true)
+
+        }
+    }
+}
