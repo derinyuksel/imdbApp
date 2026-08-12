305ad19bcfe439ce98a7efd95b9226200c7fcd7a
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-12 13:47:52 +0300

    Implement HomeScreen with UI state handling

--- app/src/main/java/com/example/imdbapp/home/HomeScreen.kt
+++ app/src/main/java/com/example/imdbapp/home/HomeScreen.kt
@@ -1,6 +1,30 @@
 package com.example.imdbapp.home
 
-class HomeScreen {
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.imdbapp.core.HomeViewModel
 
+@Composable
 
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel()
+){
+
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+    when {
+        stateisLoading -> {
+            CircularProgressIndicator()
+        }
+        state.error != null -> {
+            Text (text = state.error!!)
+        }
+        else -> {
+            Text (text = state.trendingMovies[0].title)
+            Text (text = state.trendingMovies[0].overview)
+        }
+    }
 }
