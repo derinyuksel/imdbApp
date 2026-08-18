9d64c552e3b672338dcfeb43c569be2897fcfdc4
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-18 15:10:06 +0300

    Add MovieCard component for displaying movie posters

---
+++ app/src/main/java/com/example/imdbapp/home/MovieCard.kt
@@ -0,0 +1,46 @@
+package com.example.imdbapp.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.imdbapp.model.MovieResponse
+
+@Composable
+fun MovieCard(
+    movie: MovieResponse.Result,
+    onMovieClick: (Int) -> Unit
+) {
+
+    Card(
+        modifier = Modifier
+            .width(120.dp)
+            .clickable { onMovieClick(movie.id) }
+    ) {
+
+        AsyncImage(
+            model = movie.posterPath,
+            contentDescription = movie.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .aspectRatio(2f/3f),
+            contentScale = ContentScale.Crop
+        )
+
+
+
+
+
+    }
+}
