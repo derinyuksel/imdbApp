e243bd6a7ef46f2898623980a50e45b8d9651a38
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 01:05:16 +0300

    Add movie and people fetching methods with generic API handler to Repository

--- app/src/main/java/com/example/imdbapp/core/Repository.kt
+++ app/src/main/java/com/example/imdbapp/core/Repository.kt
@@ -1,16 +1,40 @@
 package com.example.imdbapp.core
 
+import com.example.imdbapp.model.MovieResponse
 import com.example.imdbapp.model.Result
 import javax.inject.Inject
 
 class Repository @Inject constructor(val apiService: TmdbApi) {
 
-    suspend fun getTrendingMovies(): NetworkResult<List<Result>> {
+    suspend fun getTrendingMovies()= getHomeData { apiService.getTrendingMovies() }
+    suspend fun getPopularMovies()= getHomeData { apiService.getPopularMovies() }
+    suspend fun getTopRatedMovies()= getHomeData { apiService.getTopRatedMovies() }
+    suspend fun getUpcomingMovies()= getHomeData { apiService.getUpcomingMovies() }
+    suspend fun getTrendingPeople()= getHomeData { apiService.getTrendingPeople() }
+
+
+
+
+
+
+
+    suspend fun getHomeData(
+        apiCall: suspend ()-> MovieResponse
+    ) : NetworkResult<List<MovieResponse.Result>>{
         return try {
-            val response = apiService.getTrendingMovies()
+            val response = apiCall()
             NetworkResult.Success(response.results)
-        } catch (e: Exception) {
+        } catch (e: Exception){
             NetworkResult.Error(e.message ?: "Something went wrong")
         }
     }
+
+
+
+
+
+
+
+
+
 }
