516637de09fdfe13252f84037d2505aa026e9d06
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 01:57:09 +0300

    Update Result data class with nullable fields

--- app/src/main/java/com/example/imdbapp/model/Result.kt
+++ app/src/main/java/com/example/imdbapp/model/Result.kt
@@ -6,19 +6,21 @@ import kotlinx.serialization.SerialName
 
 @Serializable
 data class Result(
-    val adult: Boolean,
-    @SerialName ("backdrop_path") val backdropPath: String?,
-    @SerialName("genre_ids") val genreIds: List<Int>,
+    val adult: Boolean? = null,
+    @SerialName ("backdrop_path") val backdropPath: String? = null,
+    @SerialName("genre_ids") val genreIds: List<Int>? = null,
     val id: Int,
     @SerialName("media_type") val mediaType: String? = null,
-    @SerialName("original_language") val originalLanguage: String,
-    @SerialName("original_title") val originalTitle: String,
-    val overview: String,
-    val popularity: Double,
-    @SerialName("poster_path") val posterPath: String?,
+    @SerialName("original_language") val originalLanguage: String? = null,
+    @SerialName("original_title") val originalTitle: String? = null,
+    val overview: String? = null,
+    val popularity: Double? = null,
+    @SerialName("name") val name: String? = null,
+    @SerialName("profile_path") val profilePath: String? = null,
+    @SerialName("poster_path") val posterPath: String? = null,
     @SerialName("release_date") val releaseDate: String? = null,
-    val title: String,
-    val video: Boolean,
-    @SerialName("vote_average") val voteAverage: Double,
-    @SerialName("vote_count") val voteCount: Int
+    val title: String? = null,
+    val video: Boolean? = null,
+    @SerialName("vote_average") val voteAverage: Double? = null,
+    @SerialName("vote_count") val voteCount: Int? = null
 )
