b56f44039e534ac8a5c382d1977b8caf1e80963f
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-18 17:33:54 +0300

    Update MovieResponse to use camelCase with @SerialName

--- app/src/main/java/com/example/imdbapp/model/MovieResponse.kt
+++ app/src/main/java/com/example/imdbapp/model/MovieResponse.kt
@@ -1,11 +1,12 @@
 package com.example.imdbapp.model
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
 
 @Serializable
 data class MovieResponse(
     val page: Int,
     val results: List<Result>,
-    val total_pages: Int,
-    val total_results: Int
+    @SerialName("total_pages") val totalPages: Int,
+    @SerialName("total_results") val totalResults: Int
 )
