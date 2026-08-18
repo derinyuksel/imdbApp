5160ad2f8c7fc71e496ef0b43667a2a252cd9018
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 01:04:47 +0300

    Add movie and people endpoints to TmdbApi

--- app/src/main/java/com/example/imdbapp/core/TmdbApi.kt
+++ app/src/main/java/com/example/imdbapp/core/TmdbApi.kt
@@ -6,5 +6,19 @@ import retrofit2.http.GET
 
 interface TmdbApi {
     @GET("trending/movie/week")
-    suspend fun getTrendingMovies() : MovieResponse
-    }
+    suspend fun getTrendingMovies(): MovieResponse
+
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(): MovieResponse
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(): MovieResponse
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(): MovieResponse
+
+
+    @GET("trending/people/week")
+    suspend fun getTrendingPeople(): MovieResponse
+}
+
