fa9515d669edfe515ee74d2b064850b15bd85e38
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 01:05:59 +0300

    Update HomeUiState with movie categories and trending people

--- app/src/main/java/com/example/imdbapp/home/HomeUiState.kt
+++ app/src/main/java/com/example/imdbapp/home/HomeUiState.kt
@@ -5,5 +5,9 @@ import com.example.imdbapp.model.Result
 data class HomeUiState(
     val isLoading: Boolean = false,
     val trendingMovies: List<Result> = emptyList(),
+    val popularMovies: List<Result> = emptyList(),
+    val upcomingMovies: List<Result> = emptyList(),
+    val topRatedMovies: List<Result> = emptyList(),
+    val trendingPeople: List<Result> = emptyList(),
     val error: String? = null
 )
