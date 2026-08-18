dc122226abe2ca93a113b6c009b4da7bbd18442f
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-18 15:17:17 +0300

    Update HomeScreen to use LazyColumn and MoviesSection

--- app/src/main/java/com/example/imdbapp/home/HomeScreen.kt
+++ app/src/main/java/com/example/imdbapp/home/HomeScreen.kt
@@ -1,17 +1,22 @@
 package com.example.imdbapp.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.foundation.lazy.items
 
 @Composable
 
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
-){
+) {
 
     val state by viewModel.uiState.collectAsStateWithLifecycle()
 
@@ -19,18 +24,36 @@ fun HomeScreen(
         state.isLoading -> {
             CircularProgressIndicator()
         }
+
         state.error != null -> {
-            Text (text = state.error!!)
+            Text(text = state.error!!)
         }
+
         else -> {
-            Text (text = state.trendingMovies[0].title)
-            Text (text = state.trendingMovies[0].overview)
+            Box(modifier = Modifier.fillMaxSize()) {
+                LazyColumn() {
+
+                    val sections = listOf(
+                        "Trending" to state.trendingMovies
+                    )
+
+                    items(sections) { (title, movies) ->
+                        MoviesSection(
+                            title = title,
+                            movies = movies,
+                            onMovieClick = {}
+                            )
+                    }
+
+                }
+
+            }
         }
     }
 }
 
 @Composable
-fun HomeScreenContent(){
+fun HomeScreenContent() {
 
 }
 
