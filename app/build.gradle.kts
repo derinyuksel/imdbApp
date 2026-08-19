f111cc6d2d485c5e155855dbfe0959beb22e792f
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 09:38:07 +0300

    Add Navigation Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.play.services.basement)
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.retrofit)
     implementation(libs.kotlinx.serialization.json)
