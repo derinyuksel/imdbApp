813a1ea493cf4f175bb628f7f4da70e75f2e84c3
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 11:53:25 +0300

    Add detail fetching for movie and people credits into Repository.kt

--- app/src/main/java/com/example/imdbapp/core/Repository.kt
+++ app/src/main/java/com/example/imdbapp/core/Repository.kt
@@ -6,35 +6,40 @@ import javax.inject.Inject
 
 class Repository @Inject constructor(val apiService: TmdbApi) {
 
-    suspend fun getTrendingMovies()= getHomeData { apiService.getTrendingMovies() }
-    suspend fun getPopularMovies()= getHomeData { apiService.getPopularMovies() }
-    suspend fun getTopRatedMovies()= getHomeData { apiService.getTopRatedMovies() }
-    suspend fun getUpcomingMovies()= getHomeData { apiService.getUpcomingMovies() }
-    suspend fun getTrendingPeople()= getHomeData { apiService.getTrendingPeople() }
+    suspend fun getTrendingMovies() = getHomeData { apiService.getTrendingMovies() }
+    suspend fun getPopularMovies() = getHomeData { apiService.getPopularMovies() }
+    suspend fun getTopRatedMovies() = getHomeData { apiService.getTopRatedMovies() }
+    suspend fun getUpcomingMovies() = getHomeData { apiService.getUpcomingMovies() }
+    suspend fun getTrendingPeople() = getHomeData { apiService.getTrendingPeople() }
 
+    suspend fun getMovieDetails(id: Int) = getDetails { apiService.getMovieDetails(id) }
 
+    suspend fun getMovieCredits(id: Int) = getDetails { apiService.getMovieCredits(id) }
 
-
-
+    suspend fun getPersonMovies(id: Int) = getDetails { apiService.getPersonMovies(id) }
 
 
     suspend fun getHomeData(
-        apiCall: suspend ()-> MovieResponse
-    ) : NetworkResult<List<Result>>{
+        apiCall: suspend () -> MovieResponse
+    ): NetworkResult<List<Result>> {
         return try {
             val response = apiCall()
             NetworkResult.Success(response.results)
-        } catch (e: Exception){
+        } catch (e: Exception) {
             NetworkResult.Error(e.message ?: "Something went wrong")
         }
     }
 
-
-
-
-
-
-
+    private suspend fun <T> getDetails(
+        apiCall: suspend () -> T
+    ): NetworkResult<T> {
+        return try {
+            val response = apiCall()
+            NetworkResult.Success(response)
+        } catch (e: Exception) {
+            NetworkResult.Error(e.message ?: "Something went wrong")
+        }
+    }
 
 
 }
