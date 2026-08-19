06916d5f4aae6c537beae879e25c23d8b9d8d16e
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-19 11:52:17 +0300

    Add movie and people credits API endpoints

--- app/src/main/java/com/example/imdbapp/core/TmdbApi.kt
+++ app/src/main/java/com/example/imdbapp/core/TmdbApi.kt
@@ -1,7 +1,11 @@
 package com.example.imdbapp.core
 
+import com.example.imdbapp.model.MovieCreditsResponse
 import com.example.imdbapp.model.MovieResponse
+import com.example.imdbapp.model.PersonMovieCreditsResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
+import com.example.imdbapp.model.Result
 
 
 interface TmdbApi {
@@ -19,5 +23,19 @@ interface TmdbApi {
 
     @GET("person/popular")
     suspend fun getTrendingPeople(): MovieResponse
+
+    @GET ("movie/{movie_id}")
+    suspend fun getMovieDetails(@Path("movie_id") id: Int): Result
+
+    @GET ("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(@Path("movie_id") id: Int): MovieCreditsResponse
+
+    @GET ("person/{person_id}/movie_credits")
+    suspend fun getPersonMovies(@Path("person_id") id: Int): PersonMovieCreditsResponse
+
+
+
+
+
 }
 
