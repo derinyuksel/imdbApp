744036b5d4fcd95038cf29210dc9c19dd1a44162
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-28 10:32:08 +0300

    Add PersonDetailUiState data class

--- app/src/main/java/com/example/imdbapp/details/PersonDetailUiState.kt
+++ app/src/main/java/com/example/imdbapp/details/PersonDetailUiState.kt
@@ -1,4 +1,10 @@
 package com.example.imdbapp.details
+import com.example.imdbapp.model.Result
+import com.example.imdbapp.model.PersonDetailResponse
 
-class PersonDetailUiState {
-}
+data class PersonDetailUiState(
+    val isLoading: Boolean = false,
+    val person: PersonDetailResponse? = null,
+    val movies: List<Result> = emptyList(),
+    val error: String? = null
+)
