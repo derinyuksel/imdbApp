ec6ac5acd950345f0b2cc7011a8f07632196f48c
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-08-31 11:40:21 +0300

    Add filter state and TV shows to HomeUiState

--- app/src/main/java/com/example/imdbapp/home/HomeUiState.kt
+++ app/src/main/java/com/example/imdbapp/home/HomeUiState.kt
@@ -1,13 +1,18 @@
 package com.example.imdbapp.home
 
+import com.example.imdbapp.model.Genre
 import com.example.imdbapp.model.Result
 
 data class HomeUiState(
     val isLoading: Boolean = false,
+    val selectedType: String = "All", //Current selected filter
+    val selectedGenreId: Int? = null, // Selected genre
+    val genres: List<Genre> = emptyList(),
     val trendingMovies: List<Result> = emptyList(),
     val popularMovies: List<Result> = emptyList(),
     val upcomingMovies: List<Result> = emptyList(),
     val topRatedMovies: List<Result> = emptyList(),
-    val trendingPeople: List<Result> = emptyList(),
+    val tvShows: List<Result> =emptyList(), //List of popular movies
+    val trendingPeople: List<Result> = emptyList(), //List of popular people
     val error: String? = null
 )
