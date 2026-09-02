5ddad36074513fc053a39e4c92c9a0cbd4b0d093
derinyuksel <102306868+derinyuksel@users.noreply.github.com>
2026-09-02 13:40:24 +0300

    Cleaned unused plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    id("com.google.gms.google-services") version "4.5.0" apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
 }
